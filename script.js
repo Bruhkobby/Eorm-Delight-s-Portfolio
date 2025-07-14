b9071a56f5df9be94c8a3ba2e63b2d269b20c816
Bruhkobby <Bruhkobby707@gmail.com>
2025-07-14 00:23:57 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function toggleMenu() {
+  document.getElementById('nav').classList.toggle('active');
+}
+function openModal(id) {
+  document.getElementById(id).style.display = 'flex';
+}
+function closeModal(id) {
+  document.getElementById(id).style.display = 'none';
+}
